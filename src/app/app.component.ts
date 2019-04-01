9679f9b56a353f4c145d51f4786804c9ef94b872
santiago.yacelga <esyacelga@hotmail.com>
2019-04-01 11:13:53 -0500

    Storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import {Component} from '@angular/core';
 import {NavController, Platform} from '@ionic/angular';
 import {SplashScreen} from '@ionic-native/splash-screen/ngx';
 import {StatusBar} from '@ionic-native/status-bar/ngx';
+import {LoginStorageService} from './services/seguridad/login-storage.service';
+import {UsuarioService} from './services/seguridad/usuario.service';
 
 @Component({
     selector: 'app-root',
@@ -13,14 +15,26 @@ export class AppComponent {
         private platform: Platform,
         private splashScreen: SplashScreen,
         private statusBar: StatusBar,
-        private navCtrl: NavController
+        private navCtrl: NavController,
+        private usuarioSvc: UsuarioService,
+        private loginStorage: LoginStorageService
     ) {
         this.initializeApp();
     }
 
     initializeApp() {
         this.platform.ready().then(() => {
-            this.navCtrl.navigateRoot('signin');
+
+            this.loginStorage.cargarStorage();
+            if (this.loginStorage.user && this.loginStorage.user.correo && this.loginStorage.user.clave) {
+
+                this.usuarioSvc.setAuthenticated(true);
+                this.navCtrl.navigateRoot('home');
+            } else {
+                this.navCtrl.navigateRoot('signin');
+            }
+
+
             this.statusBar.styleDefault();
             this.splashScreen.hide();
         });
